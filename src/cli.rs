c9e296919353619ec03e95b64dbdabc481a63634
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-02-23 22:17:49 -0800

    chore(Mathical): refresh

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,3 @@
 pub fn run() {
-    let mut hello = String::from("Hello");
-
-    hello.push_str(" world");
-
-    if hello.is_empty() {
-        println!("String is empty!")
-    } else {
-        println!("String is not empty")
-    }
-
-    for word in hello.split_whitespace() {
-        println!("{}", word);
-    }
-
-    println!("{}", hello.len());
-    println!("{}", hello);
-    println!("Capacity: {}", hello.capacity());
+    
 }
