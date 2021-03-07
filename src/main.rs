ce690df043a5dce61ad4a6c836f65fd3ca08aa5e
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-07 11:46:48 -0800

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use fltk::{app, window::*};
+use fltk::{app, window::*, button::*};
 
-fn main() {
+fn create_window() {
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Mathical Graph Engine");
-    wind.end();
-    wind.show();
+    let mut win = Window::new(100, 100, 400, 300, "Mathical Graph Engine");
+    let but1 = Button::new(10, 10, 80, 40, "Button 1");
+
+    win.end();
+    win.show();
     app.run().unwrap();
 }
+
+fn process_input() {
+    let args: Vec<String> = std::env::args().collect();
+    
+    let first_argument: &str = &*args[1].to_lowercase();
+
+    match first_argument {
+        "graph" => {
+            create_window()
+        }
+
+        _ => {
+            println!("Unknown command");
+        }
+    }
+}
+fn main() {
+    process_input();
+} 
