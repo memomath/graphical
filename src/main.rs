e80daae0f9ad5a79b7c96778a97edab14e1f47dc
importly <aryankuthakur@hotmail.com>
2021-03-07 15:47:40 -0500

    Basic graph

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,18 @@ fn create_window() {
     let win_width = 500;
     let win_height = 500;
 
+    let mut num_of_lines = 5;
+    num_of_lines += 1;
+    let num_of_places = win_width/num_of_lines;
+
     let mut win = window::Window::new(100, 100, win_width, win_height, "Mathical Graph Engine");
-    win.resizable(&win);
     win.draw(move || {
+        draw::set_draw_color(Color::Blue);
+        for i in 1..num_of_lines {
+            draw::draw_line(i*num_of_places, 0, i*num_of_places, 500);
+            draw::draw_line(0, i*num_of_places, 500, i*num_of_places);
+        }
+
         draw::set_draw_color(Color::Black);
         draw::draw_line(win_width / 2, 0, win_width / 2, win_height);
         draw::draw_line(0, win_height / 2, win_width, win_height / 2);
