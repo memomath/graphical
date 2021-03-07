6d66e3bcd44c4791c69717a2005ee56ab7c5ad01
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-07 11:22:29 -0800

    feat(Mathical): basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use fltk::{app, window::*};
+
 fn main() {
-    println!("hello world");
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Mathical Graph Engine");
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
