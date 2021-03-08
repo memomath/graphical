0611ba215b0bff492158ab05870c61ec7c0265c3
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-07 20:06:09 -0800

    remove structopt

--- src/cli.rs
+++ src/cli.rs
@@ -3,11 +3,6 @@ pub mod graph;
 pub mod cli {
     use super::graph;
 
-    //help message
-    pub fn help() {
-        println!("no help for you")
-    }
-
     pub fn cli_init() {
         let args: Vec<String> = std::env::args().collect();
         let first_argument: &str = &*args[1].to_lowercase();
@@ -15,10 +10,6 @@ pub mod cli {
         match first_argument {
             "graph" => {
                 graph::graph();
-            }
-            
-            "help" => {
-                help();
             }
             _ => {
                 println!("Unknown command");
