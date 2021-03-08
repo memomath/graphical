631188a03f090d4c62fc91ea7e8227f7559c3336
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-07 20:14:15 -0800

    colors

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,8 @@
 pub mod graph;
 
 pub mod cli {
+    use colored::Colorize;
+
     use super::graph;
 
     pub fn cli_init() {
@@ -9,10 +11,11 @@ pub mod cli {
 
         match first_argument {
             "graph" => {
+                println!("{}", "Successfully graphed!".green().bold());
                 graph::graph();
             }
             _ => {
-                println!("Unknown command");
+                println!("{}: Unknown command", "ERROR:".red());
             }
         }
     }
